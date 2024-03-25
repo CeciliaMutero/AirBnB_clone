b92d01e7ad190414f4c32fa4f03dc7c011a58c37
Cecilia <cecilmutero66@gmail.com>
2024-03-24 17:30:37 -0700

    1. Be pycodestyle compliant!

---
+++ add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add_numbers(a, b):
+    """Add two numbers."""
+    return a + b
+
+
+def main():
+    """Main function."""
+    num1 = 5
+    num2 = 10
+    result = add_numbers(num1, num2)
+    print("The sum is:", result)
+
+
+if __name__ == "__main__":
+    main()
