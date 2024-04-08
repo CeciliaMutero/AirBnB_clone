546162ef8463287170c769f6c1aa3bf8a7de6e57
Cecilia <cecilmutero66@gmail.com>
2024-04-08 01:10:02 -0700

    console

--- models/user.py
+++ models/user.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """"inherits from BaseModel"""
+    """inherits from BaseModel"""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.email = kwargs.get('email', '')
